90c53dc1ee24d85f123a05cae1aaab53c4bff4eb
Dennis <dennis@kobert.dev>
2022-02-05 21:25:31 +0100

    Auto update canvas

--- graphene/src/layers/layer_info.rs
+++ graphene/src/layers/layer_info.rs
@@ -106,7 +106,7 @@ impl Layer {
 
 	pub fn transform_iter(&self) -> TransformIter<'_> {
 		TransformIter {
-			stack: vec![(self, glam::DAffine2::default(), 0)],
+			stack: vec![(self, glam::DAffine2::from_scale(DVec2::splat(0.1)), 0)],
 		}
 	}
 
@@ -283,6 +283,7 @@ impl<'a> Iterator for TransformIter<'a> {
 	fn next(&mut self) -> Option<Self::Item> {
 		match self.stack.pop() {
 			Some((layer, transform, depth)) => {
+				log::debug!("transform: {transform:?}");
 				if let LayerDataType::Folder(folder) = &layer.data {
 					let layers = folder.layers();
 					let new_transform = transform * layer.transform;
